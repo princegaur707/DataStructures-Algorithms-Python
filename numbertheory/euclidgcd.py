9da19d676a90c28c00176ea1d487334b99eec9be
proxy707 <princegaur1998@gmail.com>
2020-03-19 22:18:24 +0530

    files changed

--- numbertheory/euclidgcd.py
+++ numbertheory/euclidgcd.py
@@ -1,17 +1,12 @@
-
+#https://leetcode.com/problems/greatest-common-divisor-of-strings/
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        
-        str1Length, str2Length = len(str1), len(str2)
-        
-        if str2Length > str1Length: # Ensures the longer string is str1 and shorter or equal string is str2
-            return self.gcdOfStrings(str2, str1)
-        
-        if str1[:str2Length] == str2: # Check if shorter string (str2) is a prefix in longer string (str1)
-		
-            if str1Length == str2Length: # If str1 and str2 are of the same length then we have found the common divisor
+        l1,l2=len(str1),len(str2)
+        if(l1<l2): #Ensure length of str1 is always greater than str2
+            return self.gcdOfStrings(str2,str1)
+        if(str1[:l2]==str2): #checking str1 is having prefix as str2 or not
+            if(l1==l2):
                 return str2
-            
-            return self.gcdOfStrings(str2, str1[str2Length:]) # return the gcd of the str2 and str1 minus the prefix
-
+            else:
+                return self.gcdOfStrings(str2,str1[l2:])
         return ""
