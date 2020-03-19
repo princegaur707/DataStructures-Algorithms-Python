9d346e64dfdcc755da5865709e608a003a8962be
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-03-20 04:45:45 +0530

    Create happynumber.py

---
+++ numbertheory/happynumber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def squaresum(self,n):
+        sm = 0
+        while n!=0:
+            dig = n%10
+            sm = sm + dig**2
+            n=n//10
+        return sm
+    
+    def isHappy(self, n: int) -> bool:
+        memo = set()
+        while n != 1:
+            n = self.squaresum(n)
+            if n in memo:
+                return False
+            else:
+                memo.add(n)
+        return True
