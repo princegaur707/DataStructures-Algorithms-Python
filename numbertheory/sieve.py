94e5ce9eee1773bf9a1d206e68a740cc29970e2b
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-03-21 21:56:44 +0530

    Update sieve.py

--- numbertheory/sieve.py
+++ numbertheory/sieve.py
@@ -1,1 +1,12 @@
-
+# T.C n*log(log(n))
+def generatePrimes(n):
+    res = [True]*(n+1)
+    res[0] = False
+    res[1] = False
+    p = 2
+    while p*p <= n:
+        if res[p] == True:
+            i = p*p
+            while i <= n:
+                res[i] = False
+                i=i+p
