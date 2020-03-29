d075c0ee3f042c309669f39e42b4522d300cf89f
proxy707 <princegaur1998@gmail.com>
2020-03-29 14:33:20 +0530

    file changed

--- numbertheory/practice.py
+++ numbertheory/practice.py
@@ -1,18 +1,15 @@
+import math
+from math import floor,ceil,sqrt
 def prime(n):
     if(n<=1):
-        return("False")
+        return(False)
     if(n<=3):
-        return("True")
+        return(True)
     if(n%2==0 or n%3==0):
-        return("True")
+        return(False)
     for i in range(5,ceil(sqrt(n)),6):
         if(n%i==0 or n%(i+2)==0):
-            return()
-
-
-        
-
-
+            return(False)
 while True:
     n=int(input())
     r=prime(n)
