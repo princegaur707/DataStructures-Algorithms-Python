8de91be9446ba8da093fc4797ea34eeeb312bc4d
proxy707 <princegaur1998@gmail.com>
2020-03-29 11:28:24 +0530

    file changed

--- numbertheory/practice.py
+++ numbertheory/practice.py
@@ -1,3 +1,20 @@
+def prime(n):
+    if(n<=1):
+        return("False")
+    if(n<=3):
+        return("True")
+    if(n%2==0 or n%3==0):
+        return("True")
+    for i in range(5,ceil(sqrt(n)),6):
+        if(n%i==0 or n%(i+2)==0):
+            return()
 
-a = list(map(int,input("\nEnter the numbers : ").split()))
-print(a)
+
+        
+
+
+while True:
+    n=int(input())
+    r=prime(n)
+    print(r)
+    
