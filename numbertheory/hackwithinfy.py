1a56186e74958ec15cc2f23748cf923fb6531109
proxy707 <princegaur1998@gmail.com>
2020-03-31 00:46:22 +0530

    file added

---
+++ numbertheory/hackwithinfy.py
@@ -0,0 +1,15 @@
+def handshake(n,k):
+    cnt=0
+    for i in range(n-1):
+        if(i<k):
+            cnt+=n-(i+1)-1
+        else:
+            cnt+=n-(i+1)
+    return cnt
+while True:
+    try:
+        a,b=map(int,input("Enter n & k:   ").split())
+        print(handshake(a,b))
+    except:
+        print("That was invalid input!")
+        break
