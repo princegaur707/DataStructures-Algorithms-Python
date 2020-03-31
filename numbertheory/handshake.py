df38ac0fdb4f952c7ce7a680e643d91133cf7663
proxy707 <princegaur1998@gmail.com>
2020-03-31 11:15:22 +0530

    files changed

--- numbertheory/handshake.py
+++ numbertheory/handshake.py
@@ -1,10 +1,10 @@
 def handshake(n,k):
     cnt=0
-    for i in range(n-1):
+    for i in range(n):
         if(i<k):
-            cnt+=n-(i+1)-1
+            cnt+=(n-(i+1)-1)
         else:
-            cnt+=n-(i+1)
+            cnt+=(n-(i+1))
     return cnt
 while True:
     try:
