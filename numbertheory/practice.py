9de33acf5211824deaf7c4b5d6949ce7810dc0c4
proxy707 <princegaur1998@gmail.com>
2020-04-01 19:08:21 +0530

    files changed

--- numbertheory/practice.py
+++ numbertheory/practice.py
@@ -2,12 +2,10 @@ def fractionSubtraction(a, b):
     cnt1=(a[0]*b[1]-a[1]*b[0])
     cnt2=(a[1]*b[1])
     def gcd(cnt1,cnt2):
-        print("Gcd")
         if(cnt2==0):
             return cnt1
-    return gcd(cnt2,cnt1%cnt2)
-    print("Non gcd")
-    #g=gcd(cnt1,cnt2)
+        return gcd(cnt2,cnt1%cnt2)
+    g=gcd(cnt1,cnt2)
     return([cnt1/g,cnt2/g])
 
 p=fractionSubtraction([7,10],[3,10])
