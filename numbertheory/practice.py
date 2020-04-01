fb65e8ab080bd6ddba2ff38190139e7326a57933
proxy707 <princegaur1998@gmail.com>
2020-04-01 10:40:05 +0530

    files changed

--- numbertheory/practice.py
+++ numbertheory/practice.py
@@ -1,22 +1,14 @@
-import math
-from math import floor,ceil,sqrt
-def checkprime(n):
-    if(n<=1):
-        return False
-    if(n<=3):
-        return True
-    if(n%2==0 or n%3==0 or n%5==0):
-        return False
-    for i in range(5,ceil(sqrt(n)),6):
-        if(n%i==0 or n%(i+2)==0):
-            return False
-    return True
+def fractionSubtraction(a, b):
+    cnt1=(a[0]*b[1]-a[1]*b[0])
+    cnt2=(a[1]*b[1])
+    def gcd(cnt1,cnt2):
+        print("Gcd")
+        if(cnt2==0):
+            return cnt1
+    return gcd(cnt2,cnt1%cnt2)
+    print("Non gcd")
+    #g=gcd(cnt1,cnt2)
+    return([cnt1/g,cnt2/g])
 
-while True:
-    try:
-        n=int(input("Enter number:  "))
-        print(checkprime(n))
-    except:
-        print("That was invalid input!")
-        break
-        
+p=fractionSubtraction([7,10],[3,10])
+print(p)
