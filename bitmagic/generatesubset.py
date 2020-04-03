2c44ed2ba7b0ff60e5716449c5a2f76676769c79
proxy707 <princegaur1998@gmail.com>
2020-04-03 20:53:20 +0530

    file added

--- bitmagic/generatesubset.py
+++ bitmagic/generatesubset.py
@@ -18,12 +18,13 @@ Output:
 # T.C and S.C O(N*2^N)
 '''
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums):
         n = len(nums)
         output = []
         for i in range(2**n,2**(n+1)):
             bitmask = bin(i)[3:]
+            print(bitmask)
             output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
         return output
 ob=Solution()
-ob.subsets([1,2,3])
+print(ob.subsets([1,2,3]))
