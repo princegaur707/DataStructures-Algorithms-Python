2260a318dc85af19edf1e789eb47ff8702bb07a0
proxy707 <princegaur1998@gmail.com>
2020-04-03 21:19:35 +0530

    file changed

--- bitmagic/generatesubset.py
+++ bitmagic/generatesubset.py
@@ -21,10 +21,10 @@ class Solution:
     def subsets(self, nums):
         n = len(nums)
         output = []
-        for i in range(2**n):       # Why not 0,2**n why only this
-            bitmask = bin(i)[2:]             # Why from 3: 
+        for i in range(2**n,2**(n+1)):       # Why not 0,2**n: b/c if we take 0 it will directly take only 0 which will make index out of range 
+            bitmask = bin(i)[3:]             # from 3: as it will be starting from 16 which is 1000 so to make it 1 
             print(bitmask)
-            output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
+            output.append([nums[j] for j in range(n) if bitmask[j] == '1'])  # It will become index out of range if not taken from 16
         return output
 ob=Solution()
 print(ob.subsets([1,2,3]))
