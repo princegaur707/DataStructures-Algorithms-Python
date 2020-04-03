04073b3a5e7506b876e106ee2e3464ef9ed3810a
proxy707 <princegaur1998@gmail.com>
2020-04-03 21:48:24 +0530

    file changed

--- bitmagic/kthbitset.py
+++ bitmagic/kthbitset.py
@@ -3,7 +3,7 @@ t = int(input())
 while t:
     n,k = map(int,input().split())
     print(str(bin(n))[2:])
-    if(n & (1 << (k-1))):
+    if(n & (1 << (k-1))):    #How this & operation is performed
         print("SET")
     else:
         print("NOT SET")
