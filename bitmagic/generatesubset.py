ea226b652bd0d1bf0eb57ee551e1a58e3fc5cfa0
proxy707 <princegaur1998@gmail.com>
2020-04-03 21:14:20 +0530

    file changed

--- bitmagic/generatesubset.py
+++ bitmagic/generatesubset.py
@@ -21,8 +21,8 @@ class Solution:
     def subsets(self, nums):
         n = len(nums)
         output = []
-        for i in range(2**n,2**(n+1)):       # Why not 0,2**n why only this
-            bitmask = bin(i)[3:]
+        for i in range(2**n):       # Why not 0,2**n why only this
+            bitmask = bin(i)[2:]             # Why from 3: 
             print(bitmask)
             output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
         return output
