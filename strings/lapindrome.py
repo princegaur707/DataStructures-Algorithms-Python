7c8472fff95f734c4449c67b5cf26ac6fa1d3f43
proxy707 <princegaur1998@gmail.com>
2020-04-10 15:31:50 +0530

    file added

--- strings/lapindrome.py
+++ strings/lapindrome.py
@@ -1,16 +1,17 @@
+from collections import Counter
 t=int(input())
 while t:
     str1=input()
     l=len(str1)
     c=0
     if l%2==0:
-        if set(str1[:l//2])==set(str1[l//2:]):
+        if Counter(str1[:l//2])==Counter(str1[l//2:]):
             c=1
             print("YES")
         if(c==0):
             print("NO")
     else:
-        if set(str1[:l//2])==set(str1[l//2+1:]):
+        if Counter(str1[:l//2])==Counter(str1[l//2+1:]):
             c=1
             print("YES")
         if(c==0):
