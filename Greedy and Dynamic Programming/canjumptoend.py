329f49674b10eab3628215700045728b963579c3
proxy707 <princegaur1998@gmail.com>
2020-04-20 13:02:16 +0530

    file changed

--- Greedy and Dynamic Programming/canjumptoend.py
+++ Greedy and Dynamic Programming/canjumptoend.py
@@ -2,9 +2,11 @@
 # T.C O(n)
 # greedy 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+    def canJump(self, nums) :
         lastpos = len(nums)-1
         for i in range(lastpos,-1,-1):
             if i+nums[i]>=lastpos:
                 lastpos = i
         return lastpos == 0
+obj=Solution()
+print(obj.canJump())
