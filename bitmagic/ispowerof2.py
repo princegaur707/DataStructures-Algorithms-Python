9ba138cd00bf07b24a20d499ffa32e91f1ab8eda
proxy707 <princegaur1998@gmail.com>
2020-04-20 12:39:36 +0530

    files added

--- bitmagic/ispowerof2.py
+++ bitmagic/ispowerof2.py
@@ -1,8 +1,10 @@
 def ispoweroftwo(n):
     return n and (not(n & (n-1)))
 
-t = int(input())
-while t:
-    n = int(input())
-    print(ispoweroftwo(n))
-    t=t-1
+while True:
+    try:
+        n = int(input("Enter number:    "))
+        print(ispoweroftwo(n))
+    except:
+        print("Invalid input!")
+        break
